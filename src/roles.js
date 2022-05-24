d2ad82b81b53a063aebc30efb873415c8c2e9857
Gustavo <gutivalente@gmail.com>
2022-05-24 12:19:10 -0300

    finished project

---
+++ src/roles.js
@@ -0,0 +1,32 @@
+export const rolesDefault = [
+  {
+    name: "Assaulter",
+    description: "Those with the Assaulter role tend to dive head first into conflicts, aiming to defeat every opponent in the way. It's not uncommon for them to get screwed up because of their overdaring tendencies.",
+    value: 0
+  },
+  {
+    name: "Vanguard",
+    description: "Those with the Vanguard role are responsible for taking all the hits for the team, with the objective of protecting them from harm. They try hard not to show any weaknesses, as everyone depends on them.",
+    value: 0
+  },
+  {
+    name: "Ruiner",
+    description: "Those with the Ruiner role like to think before acting, trying to always choose the best course of action. They are glass cannons, meaning that although very valuable, they need the team covering their weaknesses all the time.",
+    value: 0
+  },
+  {
+    name: "Healer",
+    description: "Those with the Healer role are the beating heart of the party, having to make sure team members are working well and without any hindrances. They are peaceful people who dislike fighting and just want everyone to get along.",
+    value: 0
+  },
+  {
+    name: "Disruptor",
+    description: "Those with the Disruptor role sabotage their enemies to make it easier for the team to achieve victory. They are edgy and don't think about morality too much, as it would interfere with their job.",
+    value: 0
+  },
+  {
+    name: "Supporter",
+    description: "Those with the Supporter role like to assist the party indirectly to avoid getting their hands dirty. They usually prefer to be the ones behind the scenes doing what only they can do.",
+    value: 0
+  },
+];
---
+++ src/statements.js
@@ -0,0 +1,146 @@
+export const statementsDefault = [
+  {
+    text: "You like competing with others.",
+    role: "Assaulter"
+  },
+  {
+    text: "You consider yourself a selfless person.",
+    role: "Vanguard"
+  },
+  {
+    text: "You hate having to do something you know you suck at.",
+    role: "Ruiner"
+  },
+  {
+    text: "You always try to find a way to avoid conflicts.",
+    role: "Healer"
+  },
+  {
+    text: "You think playing fair in a game where everyone plays dirty is foolish.",
+    role: "Disruptor"
+  },
+  {
+    text: "You consider yourself a very creative person.",
+    role: "Supporter"
+  },
+  {
+    text: "You would rather just deal with the problem as soon as possible instead of dwelling on the details.",
+    role: "Assaulter"
+  },
+  {
+    text: "You don't bother with people confronting you if they are not a threat to anything.",
+    role: "Vanguard"
+  },
+  {
+    text: "You consider yourself a strategist and deep thinker.",
+    role: "Ruiner"
+  },
+  {
+    text: "You strive to be loved by everyone.",
+    role: "Healer"
+  },
+  {
+    text: "You like playing pranks on others.",
+    role: "Disruptor"
+  },
+  {
+    text: "You strive to be useful to others.",
+    role: "Supporter"
+  },
+  {
+    text: "You strive for more power so no one will be able to stop you from doing whatever you want.",
+    role: "Assaulter"
+  },
+  {
+    text: "You're very patient.",
+    role: "Vanguard"
+  },
+  {
+    text: "You consider yourself to have strong qualities and strong flaws, with little to no in-betweens.",
+    role: "Ruiner"
+  },
+  {
+    text: "You forgive others easily.",
+    role: "Healer"
+  },
+  {
+    text: "If you had to do something morally questionable, you could do it with a clear consciousness.",
+    role: "Disruptor"
+  },
+  {
+    text: "You love sharing what you have with others.",
+    role: "Supporter"
+  },
+  {
+    text: "You're often reckless in your decisions.",
+    role: "Assaulter"
+  },
+  {
+    text: "You're very protective of your friends and often act like a parent.",
+    role: "Vanguard"
+  },
+  {
+    text: "Uncertainty stresses you.",
+    role: "Ruiner"
+  },
+  {
+    text: "You consider yourself to be soft-hearted.",
+    role: "Healer"
+  },
+  {
+    text: "You have opinions that you think should be kept to yourself.",
+    role: "Disruptor"
+  },
+  {
+    text: "You consider yourself a reserved person.",
+    role: "Supporter"
+  },
+  {
+    text: "You are known to hold grudges.",
+    role: "Assaulter"
+  },
+  {
+    text: "You try hard to be a role model for others.",
+    role: "Vanguard"
+  },
+  {
+    text: "You think it's ok to always rely on others to do tasks you have a hard time doing.",
+    role: "Ruiner"
+  },
+  {
+    text: "You easily get worried about the well-being of others.",
+    role: "Healer"
+  },
+  {
+    text: "\"Honor\" is a meaningless concept to you.",
+    role: "Disruptor"
+  },
+  {
+    text: "You usually have a hard time keeping up with other people's pace.",
+    role: "Supporter"
+  },
+  {
+    text: "You feel you're naturally attracted to conflicts.",
+    role: "Assaulter"
+  },
+  {
+    text: "You'll always do what you think is right, even if it's the harder choice.",
+    role: "Vanguard"
+  },
+  {
+    text: "You feel inspired by smart, rational people.",
+    role: "Ruiner"
+  },
+  {
+    text: "You love being surrounded by friends.",
+    role: "Healer"
+  },
+  {
+    text: "You consider yourself an ambitious person.",
+    role: "Disruptor"
+  },
+  {
+    text: "You always want to make everyone as confortable as possible.",
+    role: "Supporter"
+  },
+];
