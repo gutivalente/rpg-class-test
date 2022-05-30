5b734f44d6e7b46447d559e1baf46c909bade2bc
Gustavo <gutivalente@gmail.com>
2022-05-30 12:11:16 -0300

    added page title and footer

--- src/App.js
+++ src/App.js
@@ -207,6 +207,11 @@ export default function App() {
           showResults ? renderResults() :
             renderStatements()
       }
+      <footer>
+        <p>
+          Gustavo Valente | <a target='_blank' rel='noreferrer' href='https://github.com/gutivalente/rpg-class-test'>Source-code</a> | <a target='_blank' rel='noreferrer' href='https://gutivalente.github.io'>Portfolio</a> | <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/gutivalente'>LinkedIn</a>
+        </p>
+      </footer>
     </>
   );
 }
